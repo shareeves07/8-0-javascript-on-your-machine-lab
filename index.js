f7bd6165db4ead01317fe620e19b8bad8eb47daf
Shamar R <reeves.shamar1@gmail.com>
2022-06-15 13:52:44 -0400

    completed lab

--- index.js
+++ index.js
@@ -4,47 +4,43 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
- function calculator() {
-    const arr = process.argv;
-    if(arr.length <= 2){
-        return "No operation provided..."
-    }
-    if(arr[2] === "plus" || arr[2] === "minus") {
-        if(arr.length <= 3){
-            return "No numbers provided..."
-        } else {
-            for(let i = 3; i < arr.length; i++){
-                if(!Number.isInteger(i)){
-                    return "No numbers provided..."
-                }
-            }
-        }
+function calculator() {
+  const arr = process.argv;
+  if (arr.length <= 2) {
+    return 'No operation provided...';
+  }
+  if (arr[2] === 'plus' || arr[2] === 'minus') {
+    if (arr.length <= 3) {
+      return 'No numbers provided...';
     } else {
-        return "Invalid operation: modulo"
-    }
-
-    if(arr[2] === "plus"){
-        let sum = 0;
-        for(let i = 3; i < arr.length; i++){
-            let result = parseInt(arr[i]); //should turn string "5" into 5
-            sum += result;
+      for (let i = 3; i < arr.length; i++) {
+        if (!Number.isInteger(i)) {
+          return 'No numbers provided...';
         }
-        return sum;
-    } else {
-
+      }
     }
-    if(arr[2] === "minus"){
-        let sum = arr[3];
-        for(let i = 4; i < arr.length; i++){
-            let result = parseInt(arr[i]); //should turn string "5" into 5
-            sum -= result;
-        }
-        return sum;
-    } else {
+  } else {
+    return 'Invalid operation: modulo';
+  }
 
+  if (arr[2] === 'plus') {
+    let sum = 0;
+    for (let i = 3; i < arr.length; i++) {
+      let result = parseInt(arr[i]); //should turn string "5" into 5
+      sum += result;
     }
+    return sum;
+  } else {
+  }
+  if (arr[2] === 'minus') {
+    let sum = arr[3];
+    for (let i = 4; i < arr.length; i++) {
+      let result = parseInt(arr[i]); //should turn string "5" into 5
+      sum -= result;
+    }
+    return sum;
+  }
 }
-
 
 // Don't change anything below this line.
 module.exports = calculator;
